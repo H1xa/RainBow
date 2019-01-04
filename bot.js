00b7dfb15b86bfa7368bf6756554e5a518adc1bf
H1xa <45233274+H1xa@users.noreply.github.com>
2019-01-04 20:07:04 +0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -2,11 +2,11 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 const suck = JSON.parse(fs.readFileSync('./suck.json', 'utf8'));
-const prefix = "r#";
+const prefix = "$";
 // captain
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag} !`);
-          client.user.setActivity("Rainbow , r#help .",{type: 'WATCHING'});
+          client.user.setActivity("Rainbow , $help .",{type: 'WATCHING'});
   
   });// captain
 client.on("message", message => {
@@ -44,13 +44,13 @@ client.on("message", message => {
     };// captain
 });// captain
 client.on("message", message => {
-  if (message.content === "r#help") {
+  if (message.content === "$help") {
       message.react('🌈')
 message.author.send(`**
-r#set 
+$set 
  - لإنشاء رتبة الرينبو وبدا الرينبو
 - To create the role of the Rainbow & Start The Rainbow
-r#inv 
+$inv 
 - لدعوة البوت
 - To Invite the bot
 // captain// captain// captain
@@ -61,6 +61,7 @@ The steps of the role did not worked .!!// captain
 1- Place the role of the Rainbow above the colors or colored ranks if it
 2- Put the bot role above the role of the Rainbow // captain
 رآبط البوت - Bot Invite link// captain
+- Hii
 **`)// captain
   }})
    client.on('message', message => {
